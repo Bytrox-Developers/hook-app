131f99458388ccb196dcd3a3721022bf521fe331
Egor M. <83420087+EgorVegas@users.noreply.github.com>
2026-06-19 10:35:36 +0300

    Fix target URL formatting in webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   // Пересылаем на твой сервер
-  const targetUrl = "https://webhook.site/e7f274f4-67f7-4855-9a73-d26badf0a6b6/" + (req.url.split('?')[1] ? '?' + req.url.split('?')[1] : '');
+  const targetUrl = "https://webhook.site/e7f274f4-67f7-4855-9a73-d26badf0a6b6" + (req.url.split('?')[1] ? '?' + req.url.split('?')[1] : '');
   
   const response = await fetch(targetUrl, {
     method: 'POST',
